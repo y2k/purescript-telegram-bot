c2f9a8a500abe92a049390f6fb8b767f9597818f
y2k <itwisterlx@gmail.com>
2020-07-02 15:18:23 +0300

    load cat's json

--- spago.dhall
+++ spago.dhall
@@ -3,7 +3,15 @@ Welcome to a Spago project!
 You can edit this file as you like.
 -}
 { name = "my-project"
-, dependencies = [ "console", "effect", "psci-support" ]
+, dependencies =
+  [ "aff"
+  , "affjax"
+  , "argonaut"
+  , "argonaut-codecs"
+  , "console"
+  , "effect"
+  , "psci-support"
+  ]
 , packages = ./packages.dhall
 , sources = [ "src/**/*.purs", "test/**/*.purs" ]
 }
--- src/Main.purs
+++ src/Main.purs
@@ -2,9 +2,29 @@ module Main where
 
 import Prelude
 
+import Affjax as AX
+import Affjax.ResponseFormat as ResponseFormat
+import Data.Argonaut.Core (Json)
+import Data.Argonaut.Decode (decodeJson)
+import Data.Either (Either(..))
+import Data.HTTP.Method (Method(..))
 import Effect (Effect)
-import Effect.Console (log)
+import Effect.Aff (launchAff)
+import Effect.Class.Console (log)
+
+-- https://www.npmjs.com/package/node-telegram-bot-api
+
+userFromJson :: Json -> Either String { data :: { image_url :: String } }
+userFromJson = decodeJson
 
 main :: Effect Unit
-main = do
-  log "🍝"
+main = void $ launchAff $ do
+  result <- AX.request (AX.defaultRequest { url = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cat", 
+                                            method = Left GET, 
+                                            responseFormat = ResponseFormat.json })
+  case result of
+    Left e -> log $ "GET /api response failed to decode: " <> AX.printError e
+    Right { body } ->
+      case userFromJson body of
+        Right x -> log $ "Image URL: " <> x.data.image_url
+        Left e -> log  $ "Error: " <> e
