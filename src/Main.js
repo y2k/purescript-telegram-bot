6714cf85405a0890aab67c1d7987335840eebbe0
y2k <itwisterlx@gmail.com>
2020-07-04 02:26:21 +0300

    fixed #1

--- src/Main.js
+++ src/Main.js
@@ -2,19 +2,13 @@
 
 const TelegramBot = require('node-telegram-bot-api');
 
-exports.getApiKey = function () {
-  return process.env.GIPHY_API_KEY
-}
-
-exports.startBotRepl = function (f) {
-  return function () {
-    const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true });
-    bot.on('message', (msg) => {
-      const chatId = msg.chat.id;
-      let eff = f(msg)();
-      eff.then(function (result) {
-        bot.sendMessage(chatId, result);
-      })
-    });
-  };
+exports.getApiKey = () => process.env.GIPHY_API_KEY
+exports.sendVideo = bot => chatId => video => () => bot.sendVideo(chatId, video);
+exports.sendMessage = bot => chatId => text => () => bot.sendMessage(chatId, text);
+exports.startBotRepl = f => () => {
+  const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true });
+  bot.on('message', msg => {
+    let eff = f({ bot: bot, chat: msg.chat.id });
+    eff().then(function (result) { result(); })
+  });
 };
--- src/Main.purs
+++ src/Main.purs
@@ -8,14 +8,17 @@ import Control.Promise as P
 import Data.Argonaut.Core (Json)
 import Data.Argonaut.Decode (decodeJson)
 import Data.Bifunctor (lmap)
-import Data.Either (Either(..), either)
+import Data.Either (Either(..))
 import Data.HTTP.Method (Method(..))
 import Effect (Effect)
-import Effect.Aff (Aff, launchAff)
+import Effect.Aff (launchAff)
 import Effect.Class (liftEffect)
 import Effect.Class.Console (log)
 
-foreign import startBotRepl :: ({ text :: String } -> Effect (P.Promise String)) -> Effect Unit
+foreign import data Bot :: Type
+foreign import sendVideo :: Bot -> String -> String -> Effect Void
+foreign import sendMessage :: Bot -> String -> String -> Effect Void
+foreign import startBotRepl :: ({ bot :: Bot, chat :: String } -> Effect _) -> Effect Unit
 foreign import getApiKey :: Effect String
 
 parseImageJson :: Json -> Either String { data :: { image_mp4_url :: String } }
@@ -25,15 +28,16 @@ getImageUrlFromResponse response =
   response
     # lmap AX.printError
     >>= (\ { body } -> parseImageJson body)
-    # either identity (\x -> x.data.image_mp4_url)
+    # map (\x -> x.data.image_mp4_url)
 
-handleMsg :: { text :: String } -> Aff String
 handleMsg msg = do
   apiKey <- liftEffect getApiKey
   result <- AX.request $ AX.defaultRequest { url = "https://api.giphy.com/v1/gifs/random?api_key=" <> apiKey <> "&tag=cat", 
                                              method = Left GET, 
                                              responseFormat = ResponseFormat.json }
-  pure $ getImageUrlFromResponse result
+  case getImageUrlFromResponse result of
+    Right video -> pure $ sendVideo msg.bot msg.chat video
+    Left error -> pure $ sendMessage msg.bot msg.chat error
 
 main :: Effect Unit
 main = void $ launchAff $ do
