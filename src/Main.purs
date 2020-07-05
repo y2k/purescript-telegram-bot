39c19d9483cf8826e718d0689b514367c45e0ffa
y2k <itwisterlx@gmail.com>
2020-07-05 18:43:50 +0300

    refactoring

--- src/Main.purs
+++ src/Main.purs
@@ -1,10 +1,12 @@
 module Main where
 
 import Prelude
+
 import Affjax as AX
 import Affjax.ResponseFormat as ResponseFormat
 import Data.Argonaut.Core (Json)
 import Data.Argonaut.Decode (decodeJson)
+import Data.Array (concat)
 import Data.Array.NonEmpty (head)
 import Data.Bifunctor (lmap)
 import Data.Either (Either(..))
@@ -13,19 +15,19 @@ import Data.Maybe (Maybe(..))
 import Data.String.Regex (match)
 import Data.String.Regex.Flags (noFlags)
 import Data.String.Regex.Unsafe (unsafeRegex)
+import Data.Traversable (sequence)
 import Effect (Effect)
-import Effect.Aff (launchAff_)
+import Effect.Aff (Aff, launchAff_)
 import Effect.Class (liftEffect)
 import Effect.Class.Console (log)
 
-foreign import data Bot :: Type
-foreign import sendVideo :: Bot -> String -> String -> Effect Void
-foreign import sendMessage :: Bot -> String -> String -> Effect Void
-foreign import startBotRepl :: ({ bot :: Bot, chat :: String, text :: String } -> Effect Unit) -> Effect Unit
-foreign import getApiKey :: Effect String
-
 parseImageJson :: Json -> Either String { data :: { image_mp4_url :: String } }
 parseImageJson = decodeJson
+
+data Cmd = 
+    SendVideo String String
+  | SendMessage String String
+  | DownloadJson String (Either AX.Error { body :: Json } -> Cmd)
 
 getImageUrlFromResponse response =
   response
@@ -33,21 +35,51 @@ getImageUrlFromResponse response =
     >>= (\ { body } -> parseImageJson body)
     # map (\x -> x.data.image_mp4_url)
 
-handleImageMsg msg = launchAff_ $ do
-  apiKey <- liftEffect getApiKey
-  result <- AX.request $ AX.defaultRequest { url = "https://api.giphy.com/v1/gifs/random?api_key=" <> apiKey <> "&tag=cat", 
-                                             method = Left GET, 
-                                             responseFormat = ResponseFormat.json }
-  case getImageUrlFromResponse result of
-    Right video -> liftEffect $ sendVideo msg.bot msg.chat video
-    Left error -> liftEffect $ sendMessage msg.bot msg.chat ("Bot error (" <> error <> ")")
+handleImageLoaded chat response =
+  case getImageUrlFromResponse response of
+    Right url -> SendVideo chat url
+    Left error -> SendVideo chat error
 
-handleMsg msg =
-  case match (unsafeRegex "/[^@]+" noFlags) msg.text >>= head of
-    Just "/cat" -> handleImageMsg msg
-    _ -> pure unit
+handleMsg { apiKey } msg =
+  let telegramCmd = match (unsafeRegex "/[^@]+" noFlags) msg.text >>= head in
+  case telegramCmd of
+    Just "/cat" -> 
+      let url = "https://api.giphy.com/v1/gifs/random?api_key=" <> apiKey <> "&tag=cat" in
+      [ DownloadJson url (handleImageLoaded msg.chat) ]
+    _ -> []
+
+foreign import data Bot :: Type
+foreign import sendVideo :: Bot -> String -> String -> Effect Void
+foreign import sendMessage :: Bot -> String -> String -> Effect Void
+foreign import startBotRepl :: ({ bot :: Bot, chat :: String, text :: String } -> Effect Unit) -> Effect Unit
+foreign import getApiKey :: Effect String
+
+executeCmd :: Bot -> Cmd -> Aff (Array Cmd)
+executeCmd bot cmd =
+  case cmd of
+    SendVideo chat url -> sendVideo bot chat url *> pure [] # liftEffect
+    SendMessage chat text -> sendMessage bot chat text *> pure [] # liftEffect
+    DownloadJson url f ->
+      do
+        result <- 
+          AX.defaultRequest { url = url, method = Left GET, responseFormat = ResponseFormat.json }
+          # AX.request
+        pure [ f (map (\r -> { body : r.body }) result) ]
+
+executeCmds :: Bot -> Array Cmd -> Aff Unit
+executeCmds bot cmds =
+  case cmds of
+    [] -> pure unit
+    _ ->
+      do
+        let effects = cmds # map (executeCmd bot)
+        newCmds <- sequence effects
+        executeCmds bot (concat newCmds)
 
 main :: Effect Unit
-main = launchAff_ $ do
-  _ <- liftEffect $ startBotRepl (\msg -> handleMsg msg)
+main = do
+  startBotRepl (\msg -> launchAff_ $ do
+    apiKey <- liftEffect getApiKey
+    let cmds = handleMsg { apiKey : apiKey } msg
+    executeCmds msg.bot cmds)
   log $ "Bot started..."
