9f5dd873684e0c37f45304b7db67d4090c8f8cfe
y2k <itwisterlx@gmail.com>
2020-07-07 15:02:27 +0300

    handle user login

--- src/Main.js
+++ src/Main.js
@@ -3,12 +3,23 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 exports.getApiKey = () => process.env.GIPHY_API_KEY
-exports.sendVideo = bot => chatId => video => () => bot.sendVideo(chatId, video);
+exports.sendVideo = bot => chatId => replyMsg => video => caption => () =>
+  bot.sendVideo(chatId, video, { caption: caption, reply_to_message_id: replyMsg });
 exports.sendMessage = bot => chatId => text => () => bot.sendMessage(chatId, text);
 exports.startBotRepl = f => () => {
   const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true });
   bot.on('message', msg => {
     console.log(msg)
-    f({ bot: bot, chat: msg.chat.id, text: msg.text || "" })()
+    try {
+      f({
+        bot: bot,
+        chat: msg.chat.id,
+        text: msg.text || "",
+        id: msg.message_id,
+        regUserName: msg.new_chat_member && msg.new_chat_member.first_name,
+      })()
+    } catch (e) {
+      console.log(e)
+    }
   });
 };
--- src/Main.purs
+++ src/Main.purs
@@ -12,6 +12,7 @@ import Data.Bifunctor (lmap)
 import Data.Either (Either(..))
 import Data.HTTP.Method (Method(..))
 import Data.Maybe (Maybe(..))
+import Data.Nullable (Nullable, toMaybe, toNullable)
 import Data.String.Regex (match)
 import Data.String.Regex.Flags (noFlags)
 import Data.String.Regex.Unsafe (unsafeRegex)
@@ -25,9 +26,9 @@ parseImageJson :: Json -> Either String { data :: { image_mp4_url :: String } }
 parseImageJson = decodeJson
 
 data Cmd =
-    SendVideo String String
+    SendVideo String (Maybe Int) String (Maybe String)
   | SendMessage String String
-  | DownloadJson String (Either AX.Error { body :: Json } -> Cmd)
+  | DownloadJson String (Either AX.Error { body :: Json } -> Array Cmd)
 
 getImageUrlFromResponse response =
   response
@@ -37,34 +38,41 @@ getImageUrlFromResponse response =
 
 onImageJsonLoaded chat response =
   case getImageUrlFromResponse response of
-    Right url -> SendVideo chat url
-    Left error -> SendVideo chat error
+    Right url -> [ SendVideo chat Nothing url Nothing ]
+    Left error -> [ SendMessage chat error ]
+
+onImageJsonLoadedForNewUser chat username msgId response =
+  case getImageUrlFromResponse response of
+    Right url -> [ SendVideo chat (Just msgId) url (Just $ username <> ", докажите что вы человек. Опишите что изображено на картинке. У вас есть 30 секунд 😸") ]
+    Left error -> []
 
 update { apiKey } msg =
-  let telegramCmd = match (unsafeRegex "/[^@]+" noFlags) msg.text >>= head in
-  case telegramCmd of
-    Just "/cat" ->
-      let url = "https://api.giphy.com/v1/gifs/random?api_key=" <> apiKey <> "&tag=cat" in
-      [ DownloadJson url (onImageJsonLoaded msg.chat) ]
-    _ -> []
+  let url = "https://api.giphy.com/v1/gifs/random?api_key=" <> apiKey <> "&tag=cat" in
+  case toMaybe msg.regUserName of
+    Just name -> [ DownloadJson url (onImageJsonLoadedForNewUser msg.chat name msg.id) ]
+    Nothing ->
+      let telegramCmd = match (unsafeRegex "/[^@]+" noFlags) msg.text >>= head in
+      case telegramCmd of
+        Just "/cat" -> [ DownloadJson url (onImageJsonLoaded msg.chat) ]
+        _ -> []
 
 foreign import data Bot :: Type
-foreign import sendVideo :: Bot -> String -> String -> Effect Void
+foreign import sendVideo :: Bot -> String -> Nullable Int -> String-> Nullable String -> Effect Void
 foreign import sendMessage :: Bot -> String -> String -> Effect Void
-foreign import startBotRepl :: ({ bot :: Bot, chat :: String, text :: String } -> Effect Unit) -> Effect Unit
+foreign import startBotRepl :: ({ bot :: Bot, chat :: String, text :: String, id :: Int, regUserName :: Nullable String } -> Effect Unit) -> Effect Unit
 foreign import getApiKey :: Effect String
 
 executeCmd :: Bot -> Cmd -> Aff (Array Cmd)
 executeCmd bot cmd =
   case cmd of
-    SendVideo chat url -> sendVideo bot chat url *> pure [] # liftEffect
+    SendVideo chat msgId url caption -> sendVideo bot chat (toNullable msgId) url (toNullable caption) *> pure [] # liftEffect
     SendMessage chat text -> sendMessage bot chat text *> pure [] # liftEffect
     DownloadJson url f ->
       do
         result <-
           AX.defaultRequest { url = url, method = Left GET, responseFormat = ResponseFormat.json }
           # AX.request
-        pure [ f (map (\r -> { body : r.body }) result) ]
+        pure $ f (map (\r -> { body : r.body }) result)
 
 executeCmds :: Bot -> Array Cmd -> Aff Unit
 executeCmds bot cmds =
