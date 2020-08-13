05dcd8c333e5d075667635b9e09f923e68d4361f
y2k <itwisterlx@gmail.com>
2020-08-14 00:50:39 +0300

    add new tag

--- test/PeriodicPostsImages.purs
+++ test/PeriodicPostsImages.purs
@@ -13,7 +13,36 @@ import TestUtils as T
 
 main :: Effect Unit
 main = do
-  runTest "PeriodicPostsImages.test" do
+  runTest "PeriodicPostsImages - no update at start" do
+    log <- T.newQueue
+    let env = 
+          { downloadText: (\x -> liftEffect $ T.unsafeReadTextFile $ "test/resources/" <> (show $ T.stringHashCode x.url) <> ".1.xml")
+          , sendVideo: (\x -> (M.unsafeToJson x >>= (\x -> T.push x log)) # liftEffect) }
+    start <- I.mkStart
+
+    _ <- launchAff_ $ start env
+
+    T.reset log
+    _ <- launchAff_ $ start env
+    logA <- T.toArray log
+    assertEqual { expected: [], actual: logA }
+
+  runTest "PeriodicPostsImages - no updates after updates" do
+    log <- T.newQueue
+    let env = 
+          { downloadText: (\x -> liftEffect $ T.unsafeReadTextFile $ "test/resources/" <> (show $ T.stringHashCode x.url) <> ".1.xml")
+          , sendVideo: (\x -> (M.unsafeToJson x >>= (\x -> T.push x log)) # liftEffect) }
+    start <- I.mkStart
+
+    _ <- launchAff_ $ start env
+    _ <- launchAff_ $ start $ env { downloadText = (\x -> liftEffect $ T.unsafeReadTextFile $ "test/resources/" <> (show $ T.stringHashCode x.url) <> ".2.xml") }
+
+    T.reset log
+    _ <- launchAff_ $ start $ env { downloadText = (\x -> liftEffect $ T.unsafeReadTextFile $ "test/resources/" <> (show $ T.stringHashCode x.url) <> ".2.xml") }
+    logA <- T.toArray log
+    assertEqual { expected: [], actual: logA }
+
+  runTest "PeriodicPostsImages - test" do
     log <- T.newQueue
     let env = 
           { downloadText: (\x -> liftEffect $ T.unsafeReadTextFile $ "test/resources/" <> (show $ T.stringHashCode x.url) <> ".1.xml")
@@ -31,8 +60,3 @@ main = do
     assertEqual 
       { expected: [ """{"chat":"-1001130908027","url":"http://img0.joyreactor.cc/pics/post/mp4/-6086130.mp4"}""" ]
       , actual: logA }
-
-    T.reset log
-    _ <- launchAff_ $ start $ env { downloadText = (\x -> liftEffect $ T.unsafeReadTextFile $ "test/resources/" <> (show $ T.stringHashCode x.url) <> ".2.xml") }
-    logA <- T.toArray log
-    assertEqual { expected: [], actual: logA }
