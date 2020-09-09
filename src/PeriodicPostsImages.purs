ffa22e503b6722fbf4639a321f6abec093e6b14c
y2k <itwisterlx@gmail.com>
2020-09-09 14:32:57 +0300

    add test

--- src/PeriodicPostsImages.purs
+++ src/PeriodicPostsImages.purs
@@ -4,6 +4,7 @@ import Prelude
 
 import Common as C
 import Data.Array as A
+import Data.Either (either)
 import Data.Int (fromString)
 import Data.Maybe (Maybe(..))
 import Data.Set (Set)
@@ -11,8 +12,9 @@ import Data.Set as Set
 import Data.String.Regex.Flags as RF
 import Data.String.Regex.Unsafe as RU
 import Effect (Effect)
-import Effect.Aff (Aff)
+import Effect.Aff (Aff, catchError, try)
 import Effect.Class (liftEffect)
+import Effect.Class.Console (log)
 import Effect.Ref (new, read, write)
 
 type State = { loaded :: Maybe Int, chats :: Set String }
@@ -32,8 +34,13 @@ mkStart = do
 
 start :: _ -> State -> Aff State
 start env state = do
-  xml <- env.downloadText { url: "http://joyreactor.cc/rss/tag/%D0%BB%D0%B8%D1%87%D0%B8%D0%BD%D0%BA%D0%B0%2B%D0%BA%D0%BE%D1%82%D1%8D" }
-  let ids = A.concatMap parseGifIds [ xml ]
+  xml <-
+    catchError
+      (env.downloadText { url: "http://joyreactor.cc/rss/tag/%D0%BB%D0%B8%D1%87%D0%B8%D0%BD%D0%BA%D0%B0%2B%D0%BA%D0%BE%D1%82%D1%8D" })
+      (\_ -> do
+        log "[ERROR] Network error while load RSS"
+        pure "")
+  let ids = parseGifIds xml
   case state.loaded of
     Nothing ->
       case C.maxInArray ids of
--- test/PeriodicPostsImages.purs
+++ test/PeriodicPostsImages.purs
@@ -3,7 +3,7 @@ module Test.PeriodicPostsImages where
 import Prelude
 
 import Effect (Effect)
-import Effect.Aff (launchAff_)
+import Effect.Aff as A
 import Effect.Class (liftEffect)
 import Main as M
 import PeriodicPostsImages as I
@@ -13,50 +13,57 @@ import TestUtils as T
 
 main :: Effect Unit
 main = do
+  runTest "PeriodicPostsImages - test errors handle" do
+    let env =
+          { downloadText: (\x -> A.throwError (A.error "fake error"))
+          , sendVideo: (\x -> pure unit) }
+    start <- I.mkStart
+    A.launchAff_ $ start env
+
   runTest "PeriodicPostsImages - no update at start" do
     log <- T.newQueue
-    let env = 
+    let env =
           { downloadText: (\x -> liftEffect $ T.unsafeReadTextFile $ "test/resources/" <> (show $ T.stringHashCode x.url) <> ".1.xml")
           , sendVideo: (\x -> (M.unsafeToJson x >>= (\x -> T.push x log)) # liftEffect) }
     start <- I.mkStart
 
-    _ <- launchAff_ $ start env
+    _ <- A.launchAff_ $ start env
 
     T.reset log
-    _ <- launchAff_ $ start env
+    _ <- A.launchAff_ $ start env
     logA <- T.toArray log
     assertEqual { expected: [], actual: logA }
 
   runTest "PeriodicPostsImages - no updates after updates" do
     log <- T.newQueue
-    let env = 
+    let env =
           { downloadText: (\x -> liftEffect $ T.unsafeReadTextFile $ "test/resources/" <> (show $ T.stringHashCode x.url) <> ".1.xml")
           , sendVideo: (\x -> (M.unsafeToJson x >>= (\x -> T.push x log)) # liftEffect) }
     start <- I.mkStart
 
-    _ <- launchAff_ $ start env
-    _ <- launchAff_ $ start $ env { downloadText = (\x -> liftEffect $ T.unsafeReadTextFile $ "test/resources/" <> (show $ T.stringHashCode x.url) <> ".2.xml") }
+    _ <- A.launchAff_ $ start env
+    _ <- A.launchAff_ $ start $ env { downloadText = (\x -> liftEffect $ T.unsafeReadTextFile $ "test/resources/" <> (show $ T.stringHashCode x.url) <> ".2.xml") }
 
     T.reset log
-    _ <- launchAff_ $ start $ env { downloadText = (\x -> liftEffect $ T.unsafeReadTextFile $ "test/resources/" <> (show $ T.stringHashCode x.url) <> ".2.xml") }
+    _ <- A.launchAff_ $ start $ env { downloadText = (\x -> liftEffect $ T.unsafeReadTextFile $ "test/resources/" <> (show $ T.stringHashCode x.url) <> ".2.xml") }
     logA <- T.toArray log
     assertEqual { expected: [], actual: logA }
 
   runTest "PeriodicPostsImages - test" do
     log <- T.newQueue
-    let env = 
+    let env =
           { downloadText: (\x -> liftEffect $ T.unsafeReadTextFile $ "test/resources/" <> (show $ T.stringHashCode x.url) <> ".1.xml")
           , sendVideo: (\x -> (M.unsafeToJson x >>= (\x -> T.push x log)) # liftEffect) }
     start <- I.mkStart
 
     T.reset log
-    _ <- launchAff_ $ start env
+    _ <- A.launchAff_ $ start env
     logA <- T.toArray log
     assertEqual { expected: [], actual: logA }
 
     T.reset log
-    _ <- launchAff_ $ start $ env { downloadText = (\x -> liftEffect $ T.unsafeReadTextFile $ "test/resources/" <> (show $ T.stringHashCode x.url) <> ".2.xml") }
+    _ <- A.launchAff_ $ start $ env { downloadText = (\x -> liftEffect $ T.unsafeReadTextFile $ "test/resources/" <> (show $ T.stringHashCode x.url) <> ".2.xml") }
     logA <- T.toArray log
-    assertEqual 
+    assertEqual
       { expected: [ """{"chat":"-1001130908027","url":"http://img0.joyreactor.cc/pics/post/mp4/-6086130.mp4"}""" ]
       , actual: logA }
